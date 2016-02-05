9a8c69e0eb8634779b0daf6a2ecd971eb1bbcabf
Graig Janssen <graig.janssen@gmail.com>
2016-02-05 11:59:02 -0500

    Back end user routes for signup, login and adding stocks

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 // MODULES //
 
-var express     = require('express'),
-    morgan      = require('morgan'),
-    mongoose    = require('mongoose'),
-    bodyParser  = require('body-parser');
+var express       = require('express'),
+    morgan        = require('morgan'),
+    mongoose      = require('mongoose'),
+    bodyParser    = require('body-parser'),
+    cookieParser  = require('cookie-parser'),
+    loadUser      = require('./middleware/loadUser');
 
 var app = express();
 
@@ -17,6 +19,10 @@ app.use(express.static('./public'));
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.use(cookieParser());
+
+app.use(loadUser);
 
 // DATABASE //
 
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,66 @@
-var express = require('express');
-var router = express.Router();
+var express   = require('express'),
+    mongoose  = require('mongoose'),
+    User      = require('../models/user'),
+    router    = express.Router();
+
+// GET ALL USERS //
+
+router.get('/', function(req, res){
+  User.find({}, function(err, dbUsers){
+    res.json({users: dbUsers});
+  });
+});
+
+// Get Current User //
+
+router.get('/current', function(req, res){
+  if (req.user) {
+    res.json( { user: req.user } );
+  } else {
+    res.json( { description: 'No User Found' } );
+  }
+});
+
+// NEW USER SIGN UP //
+
+router.post('/', function(req, res){  // POST request to /api/users
+  var newUser = new User( req.body.user );  // Make new user from sign up form
+  newUser.save(function(err, dbUser){  // Save user to the db, which will encrypt password
+    res.json(dbUser);  // Send the new user as json
+  });
+});
+
+// USER LOGIN //
+
+router.post('/authenticate', function(req, res){  // POST request to /api/users/authenticate
+  User.findOne({username: req.body.username}, function(err, dbUser){
+    if (dbUser){ // if there is a user
+      // Check password
+      dbUser.authenticate(req.body.password, function(err, isMatch){
+        if(isMatch){  // If correct
+          dbUser.setToken(err, function(){ // use setToken method
+            // Send token as json to be stored in cookie to show user is logged in
+            res.json({description: 'success', token: dbUser.token, username: dbUser.username});
+          });
+        } else {
+          res.json({description: 'invalid'});
+        }
+      });
+    } else { // if no user found, return error
+      res.json({description: 'invalid', status: 302});
+    }
+  });
+});
+
+// ADD STOCK TO WATCHLIST //
+
+router.post('/stocks', function(req, res){
+  var currentUser = req.user;
+  var stockData = req.body.stock;
+  currentUser.stocks.push(stockData);
+  currentUser.save(function(err){
+    res.json({description: 'stock saved'});
+  });
+});
 
 module.exports = router;
